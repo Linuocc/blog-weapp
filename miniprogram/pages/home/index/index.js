4f372a04fee2b751241685df820a6da3989b291a
Linuocc <2953828249@qq.com>
2020-10-12 20:32:29 +0800

    修复部分bug

--- miniprogram/pages/home/index/index.js
+++ miniprogram/pages/home/index/index.js
@@ -1,4 +1,6 @@
-const { formatTime } = require("../../../utils/util");
+const {
+  formatTime
+} = require("../../../utils/util");
 
 const cloud = wx.cloud;
 Page({
@@ -89,14 +91,7 @@ Page({
     const index = option.currentTarget.dataset.index;
     const data = this.data.articlesList[index];
     wx.navigateTo({
-      url: '/pages/home/detail/detail',
-      success(res) {
-        res.eventChannel.emit("getData", {
-          title: data.title,
-          content: data.htmlContent,
-          aid:data.aid
-        })
-      }
+      url: '/pages/home/detail/detail?aid=' + data.aid,
     })
   },
 
